37b53b91cb36336790d21f3d692d20402ca9a1d9
Wesley Ferreira <wsf.ley@gmail.com>
2019-06-23 19:18:50 -0300

    adicionando acoes para cada usuario

--- script-php/area_secreta.php
+++ script-php/area_secreta.php
@@ -2,16 +2,64 @@
 session_start();
 $permission = $_SESSION['permission'];
 
-if($permission == 1){
-    echo ("
-        <h1 style='font-size:150px;'>ESSA AREA É TREMENDAMENTE SECRETA</h1>     
-    ");
-}else{
+if ($permission == 0) {
     echo ("
     <h1 style='font-size:150px;'>VOCE ESTA TENTANDO BURLAR O SITE? SAFADINHx!</h1>     
-");
+    ");
+} else {
+    switch ($permission) {
+        case 1:
+            echo ("
+        <h1 style='font-size:150px;'>BLABLABLABLA!</h1>     
+        ");
+            break;
+        case 2:
+            echo ("
+        <h1 style='font-size:150px;'>MAIS BRABOO!</h1>     
+        ");
+            break;
+        case 3:
+            echo ("
+        <h1 style='font-size:150px;'>MAIS BRABAO!</h1>     
+        ");
+            break;
+        case 4:
+            echo ("
+        <h1 style='font-size:150px;'>O BRAD!</h1>     
+        ");
+            break;
+        case 5:
+            echo ("
+        <h1 style='font-size:150px;'>LISAO!</h1>     
+        ");
+            break;;
+        case 6:
+            echo ("
+        <h1 style='font-size:150px;'>MAIS LISOOO!</h1>     
+        ");
+            break;
+        case 7:
+            echo ("
+        <h1 style='font-size:150px;'>MACONHEIRO !</h1>     
+        ");
+            break;
+        case 8:
+            echo ("
+        <h1 style='font-size:150px;'>MAIS BRABO !</h1>     
+        ");
+            break;
+        case 9:
+            echo ("
+        <h1 style='font-size:150px;'>AMARAL BOLADAO MAIS BRABO !</h1>     
+        ");
+            break;
+        case 10:
+            echo ("
+        <h1 style='font-size:150px;'>MAIS BRABO !</h1>     
+        ");
+            break;
+        
+    }
 }
 
 session_destroy();
-
-?>
