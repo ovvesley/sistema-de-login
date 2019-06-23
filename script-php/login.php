638fa5a4ecd6e4308732e314c0e20fbd65778ea0
Wesley Ferreira <wsf.ley@gmail.com>
2019-06-23 19:20:08 -0300

    permissão de sessão para cada usuario

--- script-php/login.php
+++ script-php/login.php
@@ -22,13 +22,44 @@ function isSubscript($login_username, $login_password)
 
 $status_login = (isSubscript($login_username, $login_password));
 
-
+session_start();
 if (!$status_login) {
     echo ('<h1> LOGIN OU SENHA NÃO CADASTRADA </h1>');
     header('Location: /index.html');
     $_SESSION['permission'] = 0;
-} else {  
-    session_start();
-    $_SESSION['permission'] = 1;
+} else {
+    switch ($login_username) {
+        case "rai":
+            $_SESSION['permission'] = 1;
+            break;
+        case "dias":
+            $_SESSION['permission'] = 2;
+            break;
+        case "maia":
+            $_SESSION['permission'] = 3;
+            break;
+        case "enzo":
+            $_SESSION['permission'] = 4;
+            break;
+        case "chico":
+            $_SESSION['permission'] = 5;
+            break;
+        case "guilherme":
+            $_SESSION['permission'] = 6;
+            break;
+        case "jotape":
+            $_SESSION['permission'] = 7;
+            break;
+        case "vitin":
+            $_SESSION['permission'] = 8;
+            break;
+        case "amaral":
+            $_SESSION['permission'] = 9;
+            break;
+        case "bispo":
+            $_SESSION['permission'] = 10;
+            break;
+    }
+
     header("Location:./area_secreta.php");
 }
