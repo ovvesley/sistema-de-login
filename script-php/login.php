553a05fd187e7e1382b095b467923a8c989046de
vvesly <wsf.ley@gmail.com>
2019-06-22 23:10:58 -0300

    login side serve implement

--- script-php/login.php
+++ script-php/login.php
@@ -1,5 +1,46 @@
 <?php
+$login_password = $_POST['login_password'];
+$login_username = $_POST['login_username'];
+function exibir_mensagem($login_username)
+{
+    echo ($login_username);
+}
+
+function isSubscript($login_username, $login_password)
+{
+    $csvFile = file('dado.csv');
+    foreach ($csvFile as $line) {
+        $text = str_getcsv($line);
+        if ($login_username == $text[0] && $login_password === $text[1]) {
+            echo ('<br>' . 'USUARIO POSSUI NO REGISTRO' . '<br>');
+            return true;
+        }
+    }
+    // var_dump($data);
+    return false;
+}
+
+
+if (!isSubscript($login_username, $login_password)) {
+    echo ('<h1> LOGIN OU SENHA NÃO CADASTRADA </h1>');
+    header('Location: /index.php');
+}
 
 
 
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BEM VINDO</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
